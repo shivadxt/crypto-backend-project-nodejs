266d78a5bc7c8ffd404a047887edbe184475dc17
shivadxt <shivamweb3.0@gmail.com>
2023-12-30 19:12:07 +0530

    Created route for getting 100 Cypto details and connected with end point

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ app.listen(2000,()=> {
 app.get('/',(req,res)=>{
     res.send("Crypto Currency Converter");
 })
+
+app.get('/top100', async (req, res) => {
+    try {
+      const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+        params: {
+          vs_currency: 'usd', // You can change this to get the prices in a different currency
+          per_page: 100,
+          page: 1,
+          sparkline: false,
+        },
+      });
+  
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
---
+++ readme.md
@@ -0,0 +1,9 @@
+This is a simple cryptocurrency price conversion application:
+ - We have setup the server 
+ - Installed required packages : Express and Axios
+ - Then moved to Coin geco documentation for API tested the required end point on swagger: 
+ GET - /coins​/markets (We'll be using this to get top 100 Cryptocurrencies)
+ List all supported coins price, market cap, volume, and market related data
+- Recieved Top 100 crypto Data on our local
+![Alt text](<Screenshot from 2023-12-30 18-58-50.png>)
+
